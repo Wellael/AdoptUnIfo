9c10872acb7c60524f2bc2e1cb892d3412b4379b
Wellael <parturnax97@hotmail.com>
2016-03-02 16:25:39 +0100

    IHM

--- app/src/main/java/pandaco/adoptuninfocom/Connexion.java
+++ app/src/main/java/pandaco/adoptuninfocom/Connexion.java
@@ -2,10 +2,17 @@ package pandaco.adoptuninfocom;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Connexion extends Activity{
@@ -14,6 +21,42 @@ public class Connexion extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.connexion);
 
+        Button connexion =(Button) findViewById(R.id.connexion);
+        Button retour =(Button) findViewById(R.id.retour);
+
+        final EditText tel=(EditText) findViewById(R.id.tel);
+        final EditText mdp=(EditText) findViewById(R.id.mdp);
+
+        connexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isTelValid(String.valueOf(tel.getText()))==false){
+                    tel.setError("Le telephone n'est pas au bon format");
+                }
+            }
+        });
+
+        retour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToResult = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(goToResult);
+            }
+        });
+
+    }
+
+    public boolean isTelValid(String tel){
+
+        String pattern="^0[0-9]([0-9]{2}){4}$";
+        Pattern regEx = Pattern.compile(pattern);
+        final Matcher m = regEx.matcher(tel);
+
+        if(m.find()){
+            return true;
+        }else{
+            return false;
+        }
 
     }
 }
--- app/src/main/java/pandaco/adoptuninfocom/MainActivity.java
+++ app/src/main/java/pandaco/adoptuninfocom/MainActivity.java
@@ -65,3 +65,5 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 }
+
+//Seterror
