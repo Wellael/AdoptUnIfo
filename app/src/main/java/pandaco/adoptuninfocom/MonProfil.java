e307450f455f9e28e6b2a46ef849b3939e9a0b89
Wellael <parturnax97@hotmail.com>
2016-03-09 14:56:08 +0100

    Yolo

---
+++ app/src/main/java/pandaco/adoptuninfocom/MonProfil.java
@@ -0,0 +1,14 @@
+package pandaco.adoptuninfocom;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by lerouxca on 09/03/2016.
+ */
+public class MonProfil extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mon_profil);
+    }
+}
