51446a36d2726d6a2cb78f63180a58f164bd559e
Wellael <parturnax97@hotmail.com>
2016-03-14 11:10:20 +0100

    My tailor is rich

--- app/src/main/java/pandaco/adoptuninfocom/Recherche.java
+++ app/src/main/java/pandaco/adoptuninfocom/Recherche.java
@@ -1,7 +1,13 @@
 package pandaco.adoptuninfocom;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import java.util.Random;
 
 /**
  * Created by lerouxca on 14/03/2016.
@@ -10,5 +16,15 @@ public class Recherche extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recherche);
+
+        ImageButton recherche = (ImageButton) findViewById(R.id.recherche);
+
+        recherche.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random rand = new Random();
+                int n = rand.nextInt(50);
+            }
+        });
     }
 }
