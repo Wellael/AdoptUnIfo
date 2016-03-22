bdf8ac9a0b89cc151889a1b6978eeb038038ace5
pandav0x <pandav0x@hotmail.fr>
2016-03-22 15:15:16 +0100

    Sessions

--- app/src/main/java/pandaco/adoptuninfocom/Session.java
+++ app/src/main/java/pandaco/adoptuninfocom/Session.java
@@ -3,17 +3,29 @@ package pandaco.adoptuninfocom;
 /**
  * Created by lerouxca on 22/03/2016.
  */
-public class Session {
+public class Session
+{
 
-    private static int idPersonne = 0;
+    private static int idConnectedPerson = 0; //id de la personne connectee
+    private static int idPersonne = 0; //personne a trouver pour l'algo de recherche
 
     public static void setIdPersonne(int param)
     {
         Session.idPersonne = param;
     }
-
     public static int getIdPersonne()
     {
         return Session.idPersonne;
     }
+
+    public static void setIdConnectedPerson(int param)
+    {
+        Session.idConnectedPerson = param;
+    }
+
+    public static int getIdConnectedPerson()
+    {
+        return Session.idConnectedPerson;
+    }
+
 }
