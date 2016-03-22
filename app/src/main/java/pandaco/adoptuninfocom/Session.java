1f4eb80d73cb739ad141cab510b2018f158eef12
pandav0x <pandav0x@hotmail.fr>
2016-03-22 17:29:10 +0100

    Session

--- app/src/main/java/pandaco/adoptuninfocom/Session.java
+++ app/src/main/java/pandaco/adoptuninfocom/Session.java
@@ -12,13 +12,17 @@ public class Session
 
     private static int idConnectedPerson = 0; //id de la personne connectee
     private static int idPersonne = 0; //personne a trouver pour l'algo de recherche
-    public static List<Integer> kikilavu = new ArrayList<Integer>();
+    private static List<Integer> kikilavu = new ArrayList<Integer>();
+    private static List<String> infos = new ArrayList<String>();
 
     public static void setIdPersonne(int param){ Session.idPersonne = param; }
     public static int getIdPersonne(){ return Session.idPersonne; }
 
     public static void setIdConnectedPerson(int param){ Session.idConnectedPerson = param; }
     public static int getIdConnectedPerson(){ return Session.idConnectedPerson; }
+
+    public static void setInfos(List<String> lst){ Session.infos = lst; }
+    public static List<String> getInfos(){ return Session.infos; }
 
     public static List<Integer> getKikilavu() throws IOException {
         Reader r = Reader.getInstance();
